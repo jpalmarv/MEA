6c3ac9ef9854b1cc5ad3555f86fb9144c3253ce0
samonsalveb <106716153+samonsalveb@users.noreply.github.com>
2023-06-28 17:37:30 -0500

    Add files via upload

--- Con_movimiento1.py
+++ Con_movimiento1.py
@@ -16,18 +16,19 @@ for i in range(len(dg[0])):
     Pepas[i%npepas].append([dg[0][i], dg[1][i]])
 
 Pepas = np.array(Pepas)
-
+a = 2/5
+b = 0.8
 ################################# dim
 
-for i in range(10000):
-    if i % 200 == 0:
-        plt.plot(Pepas[0][i][0], Pepas[0][i][1], '.')
-        plt.plot(Pepas[1][i][0], Pepas[1][i][1], '.')
-        plt.plot(Pepas[2][i][0], Pepas[2][i][1], '.')
-        plt.plot(Pepas[3][i][0], Pepas[3][i][1], '.')
-        plt.plot(df[0][npepas:], df[1][npepas:], '.', markersize=2)
-        plt.xlim(-1, 602)
-        plt.ylim(-1, 205)
+for i in range(int(b*(len(dg[0]))/npepas)):
+    if i % 100 == 0:
+        plt.plot(Pepas[0][i][0], Pepas[0][i][1], '.',color='#1ce9c9')
+        plt.plot(Pepas[1][i][0], Pepas[1][i][1], '.',color='#b336ff')
+        plt.plot(Pepas[2][i][0], Pepas[2][i][1], '.',color='#b336ff')
+        plt.plot(Pepas[3][i][0], Pepas[3][i][1], '.',color='#ffd119')
+        plt.plot(df[0][npepas:], df[1][npepas:], '.', markersize=2,color='#6262f5')
+        plt.xlim(-1, 602*a)
+        plt.ylim(-1, 205*a*1.8)
         print(i)
         plt.savefig('gif/' + str(i) + '.png')
         plt.close()        
@@ -54,6 +55,6 @@ for x in range(1, len(archivos_ordenados)):
     leer_imagen = imageio.v2.imread(dirArchivo)
     img_array.append(leer_imagen)
 
-imageio.v2.mimwrite('Elgif.gif', img_array, 'GIF', duration=0.7) # duración original 1 
+imageio.v2.mimwrite('Elgif2.gif', img_array, 'GIF', duration=2) # duración original 1 
 
 print(archivos_ordenados)
